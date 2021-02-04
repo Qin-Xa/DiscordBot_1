13dcea92bdb64f272151bed7ee76de098702c8dc
Cody Dahms <easycorakkasan@gmail.com>
2021-02-04 10:33:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+    
+  if 'you' in message.content:
+    await message.channel.send('Go fuck yourself boomer')
+  
+
+client.run(os.getenv('TOKEN'))
